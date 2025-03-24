6dd10f9e4b7bcdd2bd982d4eea0a8869d92a5426
AidanP771 <aidanm771@outlook.com>
2025-03-24 18:35:57 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,9 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      globals: globals.browser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: {
-      js,
-    },
-    rules: {
-      indent: ["error", 2],
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      camelcase: ["warn", { properties: "never" }],
-      "new-cap": ["error", { newIsCap: true }],
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
 ]);
